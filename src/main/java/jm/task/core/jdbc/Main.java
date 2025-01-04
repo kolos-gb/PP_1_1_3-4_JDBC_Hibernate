e26fdba248b2c5044084d686338e24041b6e43c5
kolos-gb <bergdorf.kolos@gmail.com>
2025-01-04 19:12:58 +0300

    homework pp_3.1.1

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,38 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+    public static void main(String[] args) throws SQLException {
+        /**
+         * Алгоритм работы приложения:
+         *          В методе main класса Main должны происходить следующие операции:
+         *
+         *  Создание таблицы User(ов)
+         *  Добавление 4 User(ов) в таблицу с данными на свой выбор. После каждого добавления должен быть вывод в консоль (User с именем — name добавлен в базу данных)
+         *  Получение всех User из базы и вывод в консоль (должен быть переопределен toString в классе User)
+         *  Очистка таблицы User(ов)
+         *  Удаление таблицы
+         */
+
+
+        Util.getConnection();
+        UserDao userDao = new UserDaoJDBCImpl();
+
+        userDao.createUsersTable();
+
+        userDao.saveUser("Name1", "LastName1", (byte) 20);
+        userDao.saveUser("Name2", "LastName2", (byte) 25);
+        userDao.saveUser("Name3", "LastName3", (byte) 31);
+        userDao.saveUser("Name4", "LastName4", (byte) 38);
+
+        userDao.removeUserById(1);
+        userDao.getAllUsers();
+//        userDao.cleanUsersTable();
+//        userDao.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,20 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/new_tab";
+    public static final String USER = "root";
+    public static final String PASS = "34547809A";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASS);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
